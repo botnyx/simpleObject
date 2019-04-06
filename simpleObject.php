3306250ed19df0880d622317400b6133a1e86f9a
botnyx <chlsbkrs@gmail.com>
2019-04-06 23:02:57 +0200

    Create simpleObject.php

---
+++ simpleObject.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace botnyx/simpleObject
+
+class simpleObject implements simpleObjectInterface {
+	
+	use simpleObjectTrait;
+	
+  /*	
+    available Validators: 
+		
+    string,int,bool,float,array,object,
+		url,domain,tld,email,countrycode,languagecode,date,
+		lowercase,uppercase,phone,alpha,nowhitespace,ip
+	*/
+  
+	function objectProperties(){
+		throw new \Exception("Missing function 'objectProperties'");
+	}
+}
+
